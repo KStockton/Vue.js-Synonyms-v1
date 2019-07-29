c039157e577a5c8cebbbf6a136cabc07baf8c050
Michael KS <kingstockton@gmail.com>
2019-07-29 16:00:36 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form v-on:submit.prevent='handleSubmit'>
+      <label>
+        Enter Word
+        <input  v-model='text' name='search' type='text' />
+
+      </label>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// for(let requiredParameter of ['id']){
+//     if(!idInfo[requiredParameter])// checks to makesure all the required parameters are in the request body
+//       return response
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      text: '',
+      results: [],
+      error: ''
+    }
+  },
+  methods: {
+   async handleSubmit() {
+      const api = '3a24a84c-1f45-466a-b097-c4d753e588f5'
+      const url = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${this.text}?key=${api}`
+
+      const response = await fetch(url)
+      const data = await response.json()
+      
+      this.results = data[0].meta.syns[0]
+
+  console.log(data)
+    }
   }
 }
 </script>

f9a2f2558dd1500872c0ab5e547ea0de72dfbaad
Michael KS <kingstockton@gmail.com>
2019-07-29 16:57:46 -0600

    Update header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      word: ''
+    }
+  }
+  
+}
+  </script>
+
+<style scoped>
+  header {
+    background-color: #fdf9f3;
+    height: 100px;
+  }
+</style>
+

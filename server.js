0da8346945d806a8d79cf5e3aea7ea0627fadb5e
Michael KS <kingstockton@gmail.com>
2019-07-30 22:16:20 -0600

    Update dist and server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var serveStatic = require('serve-static');
 const path = require('path');
+
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+
+app.use('/', serveStatic(path.join(__dirname, "/dist")));
+var port = process.env.PORT || 8080;
+
 app.listen(port);
-console.log('Server is running on'+ port);
+
+console.log('Listening on port: '+ port);
